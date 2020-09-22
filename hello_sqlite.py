4c1050a852db04c2542b2da449e3e339069adbcf
“jy8474np” <jy8474np@go.minnstate.edu>
2020-09-22 17:22:33 -0500

    hello_sqlite first program run

---
+++ hello_sqlite.py
@@ -0,0 +1,21 @@
+
+import sqlite3
+
+conn = sqlite3.connect('frist_db.sqlite') # connect/create new if it does not already exist
+
+conn.execute('CREATE TABLE products (id int, name txt)')
+
+conn.execute('INSERT INTO products values (1000, "hat")')
+conn.execute('INSERT INTO products values (1000, "jacker")')
+
+conn.commit()
+
+results = conn.execute('SELECT * FROM products')
+
+for row in results:
+    print(row)
+
+conn.close()
+
+
+
