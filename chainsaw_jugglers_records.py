f1d0fcaa37145059a25faa8807a1cf1536d48526
“jy8474np” <jy8474np@go.minnstate.edu>
2020-09-22 22:16:38 -0500

    search by function added, complete, and functional

--- chainsaw_jugglers_records.py
+++ chainsaw_jugglers_records.py
@@ -26,17 +26,33 @@ def add_new_record_holder():
         conn.execute(f'INSERT INTO rankings VALUES (? , ?, ?)', (new_name, new_country, new_catches)) # Using parameterized queries to combine data and SQL statements
     conn.close()
 
+def search_by_record_holder():
+    search_name = (input('Enter the name of the record holder whose information you would like to view: '))
+    conn = sqlite3.connect(db) # Connect to the database
+    results = conn.execute('SELECT * FROM products WHERE name like ?', (search_name, ))
+    first_row = results.fetchone()
+    if first_row:
+        print('Here are the details for the record holder you searched: ', first_row)
+    else:
+        print('We were unable to find a record holder by that name in our database.')
+    conn.close()
+
 def update_record_holder():
     update_name = (input('Enter the name of the record holder you would like to update: '))
     update_catches = int(input("Enter record holder's updated number of catches: "))
-
     with sqlite3.connect(db) as conn:
         conn.execute('UPDATE rankings SET catches = ? WHERE name = ?', (update_catches, update_name))
+    conn.close()
+
+def delete_record_holder():
+    delete_name = (input('Enter the name of the record holder you would like to delete: '))
+    with sqlite3.connect(db) as conn:
+        conn.execute('DELETE from rankings WHERE name = ?', (delete_name,))
     conn.close()
 
 create_table()
 existing_record_holders()
 add_new_record_holder()
+search_by_record_holder()
 update_record_holder()
-
-
+delete_record_holder()
