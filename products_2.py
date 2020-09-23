a15a75277387af0c6a6d7058c4d31ea2c03cc541
“jy8474np” <jy8474np@go.minnstate.edu>
2020-09-22 19:42:50 -0500

    Commit for working exception handler

--- products_2.py
+++ products_2.py
@@ -4,14 +4,23 @@ import sqlite3
 db = 'products.sqlite'
 
 with sqlite3.connect(db) as conn:
-    conn.execute('CREATE TABLE IF NOT EXISTS products (name TEXT UNIQUE, quantity INT')
-conn.commit()
+    conn.execute('CREATE TABLE IF NOT EXISTS products (name TEXT UNIQUE, quantity INT)') # Adds unique constraints to variables
 conn.close()
 
-name = 'scarf'
-quantity = 2
+name = 'hat'
+quantity = 4
 
-with sqlite3.connect(db) as conn:
-    conn.execute('INSERT INTO products VALUES (?, ?)', (name, quantity))
-conn.commit()
-conn.close()
+try:
+    with sqlite3.connect(db) as conn:
+        conn.execute('INSERT INTO products VALUES (?, ?)', (name, quantity) )
+    conn.close()
+except Exception as e:
+    print('Error inserting ', e)
+
+conn = sqlite3.connect(db)
+results = conn.execute('SELECT * FROM products')
+
+for row in results:
+    print(row)
+
+print('end of program!')
