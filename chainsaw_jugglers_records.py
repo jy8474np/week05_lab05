6d59a23efd656765b9acc6f9f52adc1e3bff88c0
“jy8474np” <jy8474np@go.minnstate.edu>
2020-09-22 22:43:30 -0500

    Commit with if else for inputs and before delete update attempt.

--- chainsaw_jugglers_records.py
+++ chainsaw_jugglers_records.py
@@ -1,13 +1,15 @@
 
 import sqlite3
 
-db = 'record_holders_db.sqlite'
+db = 'record_holders_db.sqlite' # Assign variable db to database path for ease
 
+# Create a table called rankings, if it doesn't already exist
 def create_table():
     with sqlite3.connect(db) as conn:
         conn.execute('CREATE TABLE IF NOT EXISTS rankings (name text, country text, catches int)')
     conn.close()
 
+# Populate rankings table with existing rankings data
 def existing_record_holders():
     with sqlite3.connect(db) as conn:
         conn.execute('INSERT INTO rankings values ("Jane Mustonen", "Finland", 98)')
@@ -16,34 +18,43 @@ def existing_record_holders():
         conn.execute('INSERT INTO rankings values ("Chad Taylor", "USA", 78)')
     conn.close()
 
+# Obtain data for all columns via user input prompts and update rankings table with new info
 def add_new_record_holder():
-    # Inputs below prompt user for new info to add
     new_name = (input("Enter record holder's name: "))
     new_country = (input("Enter record holder's country of origin: "))
     new_catches = int(input("Enter record holder's highest number of catches: "))
-    # Inserts user input info into record_holders table of first_db
+    # Inserts user input info into a new row within the record_holders table
     with sqlite3.connect(db) as conn:
-        conn.execute(f'INSERT INTO rankings VALUES (? , ?, ?)', (new_name, new_country, new_catches)) # Using parameterized queries to combine data and SQL statements
+        conn.execute(f'INSERT INTO rankings VALUES (? , ?, ?)', (new_name, new_country, new_catches))
     conn.close()
 
+# Obtain name for desired record holder via user input prompt and display record holder's most up-to-date info
 def search_by_record_holder():
     search_name = (input('Enter the name of the record holder whose information you would like to view: '))
-    conn = sqlite3.connect(db) # Connect to the database
-    results = conn.execute('SELECT * FROM products WHERE name like ?', (search_name, ))
-    first_row = results.fetchone()
+    conn = sqlite3.connect(db)
+    search_results = conn.execute('SELECT * FROM rankings WHERE name like ?', (search_name, )) # Search table by name
+    first_row = search_results.fetchone()
+    # Display info if record holder found, display error if record holder not found
     if first_row:
         print('Here are the details for the record holder you searched: ', first_row)
     else:
         print('We were unable to find a record holder by that name in our database.')
     conn.close()
 
+# Obtain name of desired record holder via user input prompt and update catches if found
+# If record holder is not found, display error message
+
 def update_record_holder():
     update_name = (input('Enter the name of the record holder you would like to update: '))
-    update_catches = int(input("Enter record holder's updated number of catches: "))
-    with sqlite3.connect(db) as conn:
-        conn.execute('UPDATE rankings SET catches = ? WHERE name = ?', (update_catches, update_name))
+    if update_name:
+        update_catches = int(input("Enter record holder's updated number of catches: "))
+        with sqlite3.connect(db) as conn:
+            conn.execute('UPDATE rankings SET catches = ? WHERE name = ?', (update_catches, update_name))
+    else:
+        print('We were unable to find a record holder by that name in our database.')
     conn.close()
 
+# Obtain name of desired record holder via user input prompt. Delete if found, error if not found
 def delete_record_holder():
     delete_name = (input('Enter the name of the record holder you would like to delete: '))
     with sqlite3.connect(db) as conn:
